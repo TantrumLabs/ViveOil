07bf476d12ba1399f780ce933aa9fdec61337ce5
Eric Mouledoux <EricMouledoux@gmail.com>
2016-09-26 11:58:41 -0500

    Extinguisher and Fire Logic

---
+++ Assets/Scripts/ExtinguisherController.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtinguisherController : MonoBehaviour
+{
+    void Start()
+    {
+        SetSpray(false);
+    }
+
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SetSpray(true);
+        }
+        
+        else if(Input.GetKeyUp(KeyCode.Space))
+        {
+            SetSpray(false);
+        }
+	}
+    
+    public void SetSpray(bool active)
+    {
+        SprayNozzle.enabled = active;
+        
+        switch(active)
+        {
+            case true:
+                SprayFoam.Play();
+                break;
+            case false:
+                SprayFoam.Stop();
+                break;
+        }
+    }
+
+    public Collider SprayNozzle;
+    public ParticleSystem SprayFoam;
+}
---
+++ Assets/Scripts/Fire.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour
+{
+	void Update ()
+    {
+        if (transform.localScale.magnitude < 0.1f)
+            Destroy(gameObject);
+
+        if(transform.localScale.magnitude < Mathf.Sqrt(3))
+        {
+            transform.localScale += (transform.localScale * (Time.deltaTime / 2));
+        }
+	}
+}
