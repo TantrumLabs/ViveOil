d434546c7393e6c5c6d71af740251827e9eca29c
Eric Mouledoux <EricMouledoux@gmail.com>
2016-09-27 09:56:46 -0500

    Extinguisher effect modable

--- Assets/Scripts/ExtinguisherSpray.cs
+++ Assets/Scripts/ExtinguisherSpray.cs
@@ -5,6 +5,10 @@ public class ExtinguisherSpray : MonoBehaviour
 {
     void OnTriggerStay(Collider other)
     {
-        other.gameObject.transform.localScale -= (other.gameObject.transform.localScale * Time.deltaTime);
+        other.gameObject.transform.localScale -=
+            ((other.gameObject.transform.localScale * Time.deltaTime) * (CorrectExtinguisher ? 1 : -1));
+
     }
+
+    public bool CorrectExtinguisher;
 }
