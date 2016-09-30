e487ef28c13bfce30559a48a3ed2ebd2aa593a9b
Zac King <ZacKingx@gmail.com>
2016-09-30 17:43:03 -0500

    End of day 30.9.2016
    -extinguisher animation
    -teleport
    -demo scene

---
+++ Assets/Scripts/FireExtinguisher.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireExtinguisher : MonoBehaviour
+{
+    public Collider SprayNozzle;
+    public ParticleSystem SprayFoam;
+    [SerializeField] SteamVR_TrackedController LeftController;
+    //bool triggerDown = false;
+
+    void Start()
+    {
+        SetSpray(false);
+    }
+
+    void Update()
+    {
+        if (LeftController.triggerPressed)
+        {
+            SetSpray(true);
+        }
+
+        else
+        {
+            SetSpray(false);
+        }
+    }
+
+    public void SetSpray(bool active)
+    {
+        SprayNozzle.enabled = active;
+
+        switch (active)
+        {
+            case true:
+                SprayFoam.Play();
+                break;
+            case false:
+                SprayFoam.Stop();
+                break;
+        }
+    }
+}
---
+++ Assets/ViveHand.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViveHand : MonoBehaviour
+{
+    GameObject viveControllerModel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void onTriggerStay(Collider c)
+    {
+
+    }
+}
