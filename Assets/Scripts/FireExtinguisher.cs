aa23d45c5525607646abf356833f5cb3efc4f32b
Zac King <ZacKingx@gmail.com>
2016-10-03 11:16:18 -0500

    Haptic Extinguisher

--- Assets/Scripts/FireExtinguisher.cs
+++ Assets/Scripts/FireExtinguisher.cs
@@ -5,19 +5,26 @@ public class FireExtinguisher : MonoBehaviour
 {
     public Collider SprayNozzle;
     public ParticleSystem SprayFoam;
-    [SerializeField] SteamVR_TrackedController LeftController;
-    //bool triggerDown = false;
+    [SerializeField] SteamVR_TrackedController leftController;
+    [SerializeField] SteamVR_TrackedController rightController;
+
+    SteamVR_TrackedObject trackedObject;
+    SteamVR_Controller.Device device;
 
     void Start()
     {
         SetSpray(false);
+        trackedObject = rightController.gameObject.GetComponent<SteamVR_TrackedObject>();
     }
 
     void Update()
     {
-        if (LeftController.triggerPressed)
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+
+        if (leftController.triggerPressed)
         {
             SetSpray(true);
+            device.TriggerHapticPulse(500);
         }
 
         else
--- Assets/ViveHand.cs
+++ Assets/ViveHand.cs
@@ -3,20 +3,37 @@ using System.Collections;
 
 public class ViveHand : MonoBehaviour
 {
+    [SerializeField] bool hapticOnStay;
+
+
     GameObject viveControllerModel;
+    SteamVR_Controller.Device device;
+    SteamVR_TrackedObject trackedObject;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
 
-    void onTriggerStay(Collider c)
+    void onTriggerEnter(Collider c)
     {
+        if (c.tag == "Interactable")
+        {
+            HapticPulse();
+               
+        }
+    }
 
+    void HapticPulse()
+    {
+        device = SteamVR_Controller.Input((int)trackedObject.index);
+        device.TriggerHapticPulse(500); // 
     }
 }
