078a36dbc1ed05a4cffeed0afce8099e50ed1143
Zac King <ZacKingx@gmail.com>
2016-10-07 12:42:40 -0500

    eric's rebuild

--- Assets/Scripts/Interfaces/IInteractableObject.cs
+++ Assets/Scripts/Interfaces/IInteractableObject.cs
@@ -1,9 +1,9 @@
 ﻿public interface IInteractableObject
 {
     bool inUse { get; set; }
-    bool isUsable { get; set; }
-    bool highlightOnTouch { get; set; }
 
-    void Touch(bool beingTouched);
-    void Interact();
+    void PickUp();
+    void PutDown();
+
+
 }
