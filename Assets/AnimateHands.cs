af3c0810359281b6c8e99fe80d17c5fd05bd3311
Zac King <ZacKingx@gmail.com>
2017-01-06 12:01:18 -0600

    Gloves

---
+++ Assets/AnimateHands.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimateHands : MonoBehaviour {
+    /*Test*/[SerializeField]SteamVR_TrackedController control;
+
+    [SerializeField] GameObject m_Hand;
+    Animator m_Anim;
+
+	// Use this for initialization
+	void Start () {
+        m_Anim = m_Hand.GetComponent<Animator>();
+	}
+
+    void Update()
+    {
+        m_Anim.SetBool("Grab", control.triggerPressed);
+        m_Anim.SetBool("Point", control.padPressed);
+    }
+}
