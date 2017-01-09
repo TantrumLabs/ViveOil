9182e50a302760fa2ee8bb2d0f765746f033d8df
Eric Mouledoux <EricMouledoux@gmail.com>
2017-01-09 16:27:36 -0600

    Disposal script

---
+++ Assets/Scripts/ExtinguisherDisposal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExtinguisherDisposal : MonoBehaviour
+{
+    void OnTriggerStay(Collider other)
+    {
+
+        IInteractableObject i = other.GetComponent<IInteractableObject>();
+        if (i != null)
+        {
+            i.Interact();
+        }
+    }
+}
