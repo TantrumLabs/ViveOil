619dbbcd41154937b771eb81f96f81536e2f3edb
Eric Mouledoux <EricMouledoux@gmail.com>
2017-01-09 17:48:25 -0600

    new hand testing

--- Assets/Scripts/Hand.cs
+++ Assets/Scripts/Hand.cs
@@ -3,19 +3,33 @@ using System.Collections;
 
 public class Hand : MonoBehaviour
 {
-    public bool m_IsFull = false;
+    public PickUp m_InHand;
 
     public int Interact()
     {
         return 0;
     }
 
-    public int PickUp()
+    public int PickUp(PickUp aObject)
     {
+        m_InHand = aObject;
+        Instantiate(aObject.m_ObjectInHand, transform);
+
+        return 0;
+    }
+    
+    public int Drop()
+    {
+        for(int i = 0; i < transform.childCount; ++i)
+        {
+            if (transform.GetChild(i) == m_InHand.m_ObjectInHand)
+                transform.GetChild(i).transform.parent = null;
+        }
+
         return 0;
     }
 
-    public int Drop()
+    public int ResetInHand()
     {
         return 0;
     }
