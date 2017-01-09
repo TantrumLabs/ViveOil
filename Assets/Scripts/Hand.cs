cfae2809cfc368393a78ea417316a5af6977c893
Eric Mouledoux <EricMouledoux@gmail.com>
2017-01-09 16:59:33 -0600

    New script to manage hands

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hand : MonoBehaviour
+{
+    public bool m_IsFull = false;
+
+    public int Interact()
+    {
+        return 0;
+    }
+
+    public int PickUp()
+    {
+        return 0;
+    }
+
+    public int Drop()
+    {
+        return 0;
+    }
+}
