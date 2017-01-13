dd9a1f87aa256159a7fa7d3304ba49a881ccbef7
Zac King <ZacKingx@gmail.com>
2017-01-13 11:02:37 -0600

    Test Mini Game with Vive

--- Assets/Lever.cs
+++ Assets/Lever.cs
@@ -12,17 +12,16 @@ public class Lever : MonoBehaviour
     [SerializeField]
     float m_max = 0;
 
-    int percent = 0;
+    float percent = 0;
 
     void FixedUpdate()
     {
         float current = ( 360 / Mathf.PI) * gameObject.transform.localRotation.x;
-        percent = (int)(((current - m_min) / (m_max - m_min)) * 100);
-
-
+        percent = (current - m_min) / (m_max - m_min);
+        text.text = percent.ToString();
     }
 
-    int CurrentPercent()
+    public float CurrentPercent()
     {
         return percent;
     }
