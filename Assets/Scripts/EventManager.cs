846ea80ebc1842025c819e27371940f899cece90
Eric Mouledoux <EricMouledoux@gmail.com>
2017-01-19 11:31:17 -0600

    comments

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -1,50 +1,84 @@
-﻿using UnityEngine;
+﻿/* * * * *
+ * Creator: Eric Z Mouledoux
+ * Contact: Eric@TantrumLab.com
+ * 
+ * Summary:
+ * 
+ * 
+ * Usage:
+ * 
+ * 
+ * Notes:
+ * 
+ * 
+ * * * * */
+
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
 public class EventManager : MonoBehaviour
 {
+    /// <summary>
+    /// Timer for delaying event excution
+    /// </summary>
     private float m_Timer = 0;
+
+    /// <summary>
+    /// The current event (the one that just ran)
+    /// </summary>
     private TriggeredEvent m_CurrentEvent;
-    public List<TriggeredEvent> m_Events = new List<TriggeredEvent>();
+
+    /// <summary>
+    /// List of all the events
+    /// </summary>
+    public List<TriggeredEvent> m_Events
+        = new List<TriggeredEvent>();
 
     void Awake()
     {
-        m_CurrentEvent = m_Events[0];
+        m_CurrentEvent = m_Events[0];   // Sets the current event to the first event
     }
 
     void Update()
     {
-        if (m_CurrentEvent.nextEventDelay < 0)
-        {
-            return;
+        if (m_CurrentEvent.nextEventDelay < 0)          // If the current delay time is less than 0
+        {                                                   //
+            return;                                         // break out of the update loop
         }
 
-        else
-        {
-            m_Timer += Time.deltaTime;
+        else                                            // Else (the delay is >= 0)
+        {                                                   //
+            m_Timer += Time.deltaTime;                      // Increase timer by delta time
 
-            if(m_Timer >= m_CurrentEvent.nextEventDelay)
-            {
-                TriggerNextEvent();
-                m_Timer = 0;
+            if(m_Timer >= m_CurrentEvent.nextEventDelay)    // If timer is >= the current delay
+            {                                                   //
+                TriggerNextEvent();                             // Trigger the next event
+                m_Timer = 0;                                    // Reset the timer
             }
         }
     }
 
+    /// <summary>
+    /// Runs the next event in the list
+    /// </summary>
     public void TriggerNextEvent()
     {
-        int cIndex = m_Events.FindIndex(i => i == m_CurrentEvent);
-        TriggerEventAtIndex(cIndex + 1);
+        int cIndex = m_Events.FindIndex(i => i == m_CurrentEvent);  // The next event index is the current index +1
+        TriggerEventAtIndex(cIndex + 1);                            // Run the event at the next index
     }
 
+    /// <summary>
+    /// Skips forward or backwards to a specific event
+    /// </summary>
+    /// <param name="aIndex">Index of the event to call</param>
     public void TriggerEventAtIndex(int aIndex)
     {
-        if (aIndex >= m_Events.Count)
-            return;
+        if (aIndex >= m_Events.Count)       // Checks if the index passed is within the list
+            return;                             // If not, do nothing
 
-        m_CurrentEvent = m_Events[aIndex];
-        m_CurrentEvent.RunEvent();
+        m_CurrentEvent = m_Events[aIndex];  // Set the current event to the new event
+        m_CurrentEvent.RunEvent();          // Run the new event
     }
 
 }
@@ -52,9 +86,20 @@ public class EventManager : MonoBehaviour
 [System.Serializable]
 public class TriggeredEvent
 {
+    /// <summary>
+    /// Unity event linked to this custom event
+    /// </summary>
     public UnityEngine.Events.UnityEvent TriggerEvent;
+
+    /// <summary>
+    /// Time to wait after this event to run the next one
+    /// </summary>
     public float nextEventDelay;
 
+    /// <summary>
+    /// Runs the event linked to this class
+    /// </summary>
+    /// <returns></returns>
     public int RunEvent()
     {
         TriggerEvent.Invoke();
